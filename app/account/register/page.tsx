7b22467abcc3cb965408bf3385c65b8573f798c7
weiwang0305 <weiwang0305@gmail.com>
2024-01-05 13:37:52 -0500

    finished design of create account

--- app/account/register/page.tsx
+++ app/account/register/page.tsx
@@ -1,14 +1,17 @@
 const SignUpPage = () => {
   return (
     <div className='w-full h-full'>
-      <div className='flex justify-center'>
-        <div className='flex flex-col m-auto gap-5'>
-          <div>Create account</div>
+      <div className='flex justify-center p-5 mt-10'>
+        <div className='flex flex-col m-auto gap-5 px-20 py-5 w-[500px]'>
+          <div className='text-center font-bold text-4xl'>Create account</div>
           <input placeholder='First name'></input>
           <input placeholder='Last name'></input>
           <input placeholder='Email'></input>
           <input placeholder='Password'></input>
-          <button>Create</button>
+
+          <button className='m-auto border rounded-lg w-[100px] h-[40px] bg-[#22223b] text-white'>
+            Create
+          </button>
         </div>
       </div>
     </div>
