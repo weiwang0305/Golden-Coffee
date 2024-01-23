3a02302aec5680dc0594bee3891be29a89c4cd24
weiwang0305 <weiwang0305@gmail.com>
2024-01-23 15:26:50 -0500

    added type to e

--- app/components/contactPageView.tsx
+++ app/components/contactPageView.tsx
@@ -9,7 +9,7 @@ const pacifico = Pacifico({
 });
 
 const ContactPageView = () => {
-  const handleContactSubmit = (e) => {
+  const handleContactSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const inputs = document.querySelectorAll('input');
     const message = document.querySelector('textarea');
