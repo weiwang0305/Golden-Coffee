40e34359d392fac327fca07abda3665c8b376c99
weiwang0305 <weiwang0305@gmail.com>
2024-01-29 14:57:35 -0500

    added public routes, authRoutes, apiauthprefix, and default login redirect

--- routes.ts
+++ routes.ts
@@ -1,3 +1,35 @@
-//An array of routes that are accessible to the public
+/**
+ * An array of routes that are accessible to the public
+ * These routes do not require authentication
+ * @type {string[]}
+ */
 
-export const publicRoutes = ['/', '/'];
+export const publicRoutes = [
+  '/',
+  '/bakery',
+  '/contact',
+  '/foodtruck',
+  '/local',
+  '/ourstory',
+  '/cafe',
+];
+
+/**
+ * An array of routes that are used for authentication
+ * These routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ['/account/login', '/auth/register'];
+
+/**
+ * The prefix for API auhentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix = '/account/auth';
+
+/**
+ * The default redirect path after logging in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
