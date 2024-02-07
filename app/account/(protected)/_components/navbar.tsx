eca2a9f7bd59638991a78eb539d147748054ca18
weiwang0305 <weiwang0305@gmail.com>
2024-02-07 15:37:25 -0500

    added responsiveness to navbar in settings

--- app/account/(protected)/_components/navbar.tsx
+++ app/account/(protected)/_components/navbar.tsx
@@ -7,7 +7,7 @@ import { usePathname } from 'next/navigation';
 const NavBar = () => {
   const pathname = usePathname();
   return (
-    <nav className='bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm'>
+    <nav className='bg-secondary flex justify-between items-center p-4 rounded-xl w-1/2 min-w-[400px] md:min-w-[600px] md:max-w-[600px] shadow-sm'>
       <div className='flex gap-x-2'>
         <Button
           asChild
