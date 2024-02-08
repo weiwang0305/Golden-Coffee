7454bb867b5b2031aa43beaa7d98f2c46aacd765
weiwang0305 <weiwang0305@gmail.com>
2024-02-08 11:47:08 -0500

    added add to cart button

--- components/menuView.tsx
+++ components/menuView.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { MenuItem } from './types';
 import Image from 'next/image';
+import { Button } from './ui/button';
 
 const MenuView = ({ id, name, description, price, type, image }: MenuItem) => {
   return (
@@ -16,9 +17,16 @@ const MenuView = ({ id, name, description, price, type, image }: MenuItem) => {
           className='p-2'
         />
       </div>
-      <p className='text-xl font-bold tracking-wider'>{name}</p>
-      <p className='text-base font-light tracking-wider'>{description}</p>
-      <p className='text-xl font-bold'>{price}</p>
+      <p className='text-2xl font-bold tracking-wider'>{name}</p>
+      <p className='text-sm font-light tracking-wider'>{description}</p>
+      <Button
+        variant='outline'
+        className='text-lg bg-black text-white m-2 uppercase p-6'
+      >
+        <span className='tracking-wider'>Add to Cart</span>
+        <span className='mx-4'>|</span>
+        <span>{price}</span>
+      </Button>
     </div>
   );
 };
